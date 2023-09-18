af69734d2c2ac83524f19ebddf6f17291c2d204f
NITINKUMAWAT0 <nitin.kumawat.jaipur.in@gmail.com>
2023-09-18 23:34:10 +0530

    changes in header section

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -18,30 +18,70 @@ const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  return <header className="header">
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+      setTimeout(() => {
+        setShowSearch(false);
+      }, 1000);
+    }
+  }
+
+const openSearch = ()=>{
+  setMobileMenu(false)
+  setShowSearch(true)
+}
+const openMobileMenu=()=>{
+  setMobileMenu(true)
+  setShowSearch(false)
+}
+
+const navigationHandler = (type) =>{
+if (type === "movie" ){
+  navigate("/explore/movie");
+} else{
+  navigate("/explore/tv");
+}
+setMobileMenu(false);
+}
+
+  return <header className={`header ${mobileMenu ? "mobileView" : ""} ${show} `}>
     <ContentWrapper>
       <div className="logo">
         <img src={logo} alt="" />
       </div>
       <ul className="menuItems">
-        <li className="menuItems">Movies</li>
-        <li className="menuItems">TV Shows</li>
-        <li className="menuItems">
-          <HiOutlineSearch />
+        <li className="menuItem" onClick={()=>navigationHandler("movie")}>Movies</li>
+        <li className="menuItem" onClick={()=>navigationHandler("tv")}>TV Shows</li>
+        <li className="menuItem">
+          <HiOutlineSearch onClick={openSearch}/>
         </li>
       </ul>
-
       <div className="mobileMenuItems">
-        <HiOutlineSearch />
+        <HiOutlineSearch onClick={openSearch} />
         {mobileMenu ? (
-          <VscChromeClose />
+          <VscChromeClose onClick={()=>
+          setMobileMenu(false)} />
         ) : (
-          <SlMenu />
+          <SlMenu onClick={openMobileMenu} />
         )}
-
 
       </div>
     </ContentWrapper>
+    { showSearch && (<div className="searchBar">
+      <ContentWrapper>
+      <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie or TV show..."
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+            />
+            <VscChromeClose onClick={()=>
+          setShowSearch(false)} />
+          </div> 
+      </ContentWrapper>
+    </div>)}
   </header>
 };
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,9 @@ HeroBanner;
 const home = () => {
   return <div className="homePage">
       <HeroBanner/>
+      <div style={{height:1000}}>
+
+      </div>
     </div>
 
 };
