3717409feabdbcbc0c27432660b03757e3327596
NITINKUMAWAT0 <nitin.kumawat.jaipur.in@gmail.com>
2023-09-20 09:26:09 +0530

    working on switch tabs

---
+++ src/pages/home/trending/Trending.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ContentWrapper from '../../../components/contentWrapper/contentWrapper';
+import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
+
+ContentWrapper
+
+const Trending = () => {
+
+    const onTabChange = (Tab) => {
+
+    }
+
+
+    return <div className="carouselSection">
+        <ContentWrapper>
+            <span className="carouselTitle">
+                Trending
+            </span>
+            <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
+        </ContentWrapper>
+    </div>
+}
+
+export default Trending
