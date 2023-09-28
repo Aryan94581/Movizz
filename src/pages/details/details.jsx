41ac9e8915c122bae243c4296e600fd36470439a
NITINKUMAWAT0 <nitin.kumawat.jaipur.in@gmail.com>
2023-09-28 23:41:12 +0530

    working on details section!!

--- src/pages/details/details.jsx
+++ src/pages/details/details.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import "./details.scss"
+import React from 'react';
+import "./details.scss";
+import useFetch from '../../components/hooks/useFetch';
+import { useParams } from 'react-router-dom';
+import DetailsBanner from './detailsBanner/DetailsBanner';
+
 const details = () => {
+  // const {mediaType,id} = useParams();
+  // const {data , loading} = useFetch(`/${mediaType}/${id}`)
   return (
-    <div>details</div>
+    <div>
+      <DetailsBanner/>
+    </div>
   )
 }
 
