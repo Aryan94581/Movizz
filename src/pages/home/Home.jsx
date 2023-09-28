144d58e75c0ce996b0aac4445876bc8f8f3dea7e
NITINKUMAWAT0 <nitin.kumawat.jaipur.in@gmail.com>
2023-09-28 22:46:34 +0530

    Top Rated carousel completed!!

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,13 @@ import "./style.scss";
 import HeroBanner from "./heroBanner/heroBanner";
 import Trending from "./trending/Trending";
 import Popular from "./popular/Popular";
-HeroBanner;
+import TopRated from "./topRated/TopRated";
 const home = () => {
   return <div className="homePage">
       <HeroBanner/>
       <Trending/>
       <Popular/>
+      <TopRated/>
       <div style={{height:500}}>
       </div>
     </div>
